586b633dfc4653c6e7bee5c408d2e7b68d203319
yanis lamliji <yanis.lamliji@ynov.com>
2025-04-03 11:56:47 +0200

    PlatformIo Component Folder

---
+++ Component/src/espcode.cpp
@@ -0,0 +1,25 @@
+// #include <Arduino.h>
+
+
+// void setup(){
+//   Serial.begin(9600);
+//   pinMode(led, OUTPUT);
+//   delay(1000);
+// }
+
+// void loop(){
+//   digitalWrite(led, HIGH);
+//   Serial.println("LED is on");
+//   delay(1000);
+//   digitalWrite(led, LOW);
+//   Serial.println("LED is off");
+//   delay(1000);
+// }
+
+// int setUp(int led1){
+//     digitalWrite(led1, HIGH);
+// }
+
+// void checkUp(){
+    
+// }
---
+++ Component/src/espcode.hpp
@@ -0,0 +1,21 @@
+// #ifndef Espcode
+// #define ESPCODE
+// #include <Arduino.h>
+
+// void setup();
+// void loop();
+// void checkUp();
+
+// class RFID
+// {
+// protected :
+// int pin_SDA;
+// int pin_SCK;
+// int pin_MOSI;
+// int pin_MISO;
+
+// int pin_GND;
+// int pin_RST;
+// int pin_3V;
+// }
+// #endif
